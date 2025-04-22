a2845adae14a28f8d34f1ebbe049486f77ec3b76
Arun <intformbase@gmail.com>
2025-04-22 17:48:43 +0530

    first commit

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import { useState, ChangeEvent } from "react";
+import Link from "next/link";
+
+export default function Page() {
+  const [ formData, setFormData ] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    re_password: "",
+  });
+
+  const { first_name, last_name, email, password, re_password } = formData;
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          alt="Modelflick"
+          src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+          className="mx-auto h-10 w-auto"
+        />
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+          Sign Up
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form action="#" method="POST" className="space-y-6">
+          <div>
+            <label
+              htmlFor="first_name"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              First Name
+            </label>
+            <div className="mt-2">
+              <input
+                id="first_name"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                name="first_name"
+                type="text"
+                value={first_name}
+                onChange={onChange} // Handler is here
+                required
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="last_name"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Last Name
+            </label>
+            <div className="mt-2">
+              <input
+                id="last_name"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                name="last_name"
+                type="text"
+                value={last_name}
+                onChange={onChange} // Handler is here
+                required
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Email Address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                name="email"
+                type="email"
+                value={email}
+                onChange={onChange} // Handler is here
+                required
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Password
+            </label>
+            <div className="mt-2">
+              <input
+                id="password"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                name="password"
+                type="password"
+                value={password}
+                onChange={onChange} // Handler is here
+                required
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="re_password"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Confirm Password
+            </label>
+            <div className="mt-2">
+              <input
+                id="re_password"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                name="re_password"
+                type="password"
+                value={re_password}
+                onChange={onChange} // Handler is here
+                required
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm/6 text-gray-500">
+          Already Have an Account?{" "}
+          <Link
+            href="/auth/login"
+            className="font-semibold text-indigo-600 hover:text-indigo-500"
+          >
+            Login Here
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
