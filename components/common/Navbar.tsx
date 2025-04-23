c1c481d5a821c106c39505ee3d992b11d00356f6
Arun <intformbase@gmail.com>
2025-04-23 12:48:49 +0530

    added home page

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -88,7 +88,7 @@ export default function Navbar() {
 							<div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
 								<div className='flex flex-shrink-0 items-center'>
 									<NavLink href='/' isBanner>
-										Full Auth
+										Modelflick
 									</NavLink>
 								</div>
 								<div className='hidden sm:ml-6 sm:block'>
