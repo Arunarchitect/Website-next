453320e652ee184e95d7de3a60e1a59062946892
Arun <intformbase@gmail.com>
2025-04-24 12:22:48 +0530

    cahnged dashboard dark theme

--- components/common/List.tsx
+++ components/common/List.tsx
@@ -15,12 +15,12 @@ export default function List({ config }: Props) {
 			{config.map(({ label, value }) => (
 				<li key={label} className='flex justify-between gap-x-6 py-5'>
 					<div>
-						<p className='text-sm font-semibold leading-6 text-gray-900'>
+						<p className='text-sm font-semibold leading-6 text-gray-700 dark:text-gray-300'>
 							{label}
 						</p>
 					</div>
 					<div>
-						<p className='text-sm font-semibold leading-6 text-gray-900'>
+						<p className='text-sm font-semibold leading-6 text-gray-700 dark:text-gray-300'>
 							{value || <Spinner sm />}
 						</p>
 					</div>
